6252abf128612e88104fa2773d5c472bc1aff84c
ttejeda <ttejeda@ucol.mx>
2022-03-12 21:48:33 -0600

    part 2, implemented config.js

--- emisor/app.js
+++ emisor/app.js
@@ -1,12 +1,13 @@
 //var Emitter = require('./emitter');
 var Emitter = require('events');
+var config = require('./config');
 
 var emtr = new Emitter(); // Crea un nuevo emisor de eventos
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
     console.log('Somewhere, someone said hello.');
 });
 // En esta sección se están creando los nuevos eventos, ambos son del mismo tipo y ambos devuelven una cadena por la consola
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
     console.log('A greeting occurred!');
 });
 
@@ -15,13 +16,15 @@ emtr.emit('greet');
 // Al ejecutar el evento se ejecutan los dos que se agregaron anteriormente, debido a que ambos son el mismo tipo de eventos
 
 
-emtr.on('jump', () => {
+emtr.on(config.events.JUMP, () => {
     console.log('someone jumped!');
 });
 
-console.log(emtr); // emtr contiene dos tipos de eventos: 1. greet, el cual activa 2 funciones anónimos; 2. jump, este solo activa una función anónimo
+//console.log(emtr); // emtr contiene dos tipos de eventos: 1. greet, el cual activa 2 funciones anónimos; 2. jump, este solo activa una función anónimo
 emtr.emit('jump');
 
 
 // Notas de la práctica 7
 //Pese a los cambios, la ejecución no cambió
+//Después de adaptar las llamadas de funciones, todas siguieron funcionando igual, ya que está especificado el type que utilizará
+    // y no tener que repetir la palabra 'greet' para evitar errores humanos
